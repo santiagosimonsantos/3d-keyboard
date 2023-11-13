347dfed51da71c99442cee14d4f221116a2460a2
santiagosimonsantos <santiago.simon.santos@gmail.com>
2023-11-13 21:36:50 +0100

    fix: scroolbar view

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,27 @@
 "use client"
-import Spline from '@splinetool/react-spline';
+import React, { Suspense } from 'react';
 
-export default function Home() {
+const Spline = React.lazy(() => import('@splinetool/react-spline'));
+
+export default function App() {
+  const handleKeyDown = (e) => {
+    console.log(e.key);
+    if (e.key === "Tab") {
+      e.preventDefault();
+      console.log("Se presionó la tecla tab");
+      // Aquí puedes hacer lo que quieras cuando se presiona la tecla tab
+    }
+  };
+
   return (
-    <Spline scene="https://prod.spline.design/p4yG3aeHgug7eZIF/scene.splinecode" />
+    <main>
+    <Suspense fallback={<div>Loading...</div>}>
+        <Spline
+        scene="https://prod.spline.design/p4yG3aeHgug7eZIF/scene.splinecode"
+        onKeyDown={(e) => handleKeyDown(e)}
+        className=' max-h-screen bg-zinc-800'
+        />
+    </Suspense>
+    </main>
   );
 }
-
